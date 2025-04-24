60eedb5ef2c62df2cfb9a015d843b0c2df2d195f
matheusOliv23 <matheushenri1996@gmail.com>
2025-04-24 20:04:31 -0300

    feat: add loading and not found page

---
+++ app/loading.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import loader from '@/assets/animations/loader.gif';
+import Image from 'next/image';
+
+export default function LoadingPage() {
+  return (
+    <div
+      style={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '100vh',
+        width: '100vw',
+      }}
+    >
+      <Image src={loader} alt='Loading...' width={150} height={150} />
+    </div>
+  );
+}
