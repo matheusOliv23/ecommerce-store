a6419258cdeee8b1cec9a04366d8d0d4b468c28a
matheusOliv23 <matheushenri1996@gmail.com>
2025-04-24 16:54:53 -0300

    feat: initial project config

---
+++ lib/constants/index.ts
@@ -0,0 +1,4 @@
+export const APP_NAME = 'E-commerce';
+export const APP_DESCRIPTION = 'Plataforma de ecommerce';
+export const API_URL =
+  process.env.NEXT_PUBLIC_SERVER_URL || 'http://localhost:3000';
