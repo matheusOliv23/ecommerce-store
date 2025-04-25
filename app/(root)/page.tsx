6fa4343dccbc8c1b30bc5c09077b10eaaacc7251
matheusOliv23 <matheushenri1996@gmail.com>
2025-04-25 19:11:06 -0300

    feat: add product list layout

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,5 +1,11 @@
 import React from 'react';
+import sampleData from '@/db/sample-data';
+import ProductList from '@/components/product/product-list';
 
 export default function Homepage() {
-  return <div className='text-xl'>Ecommerce</div>;
+  return (
+    <div className='text-xl'>
+      <ProductList data={sampleData?.products} title='Produtos' limit={4} />
+    </div>
+  );
 }
---
+++ components/product/product-list.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import ProductCard from './product-card';
+
+export default function ProductList({
+  data,
+  title,
+  limit,
+}: {
+  data: any;
+  title?: string;
+  limit?: number;
+}) {
+  const limitedData = limit ? data?.slice(0, limit) : data;
+  return (
+    <div className='my-10'>
+      <h2 className='h2-bold mb-4'>{title}</h2>
+      {data.length > 0 ? (
+        <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4'>
+          {limitedData.map((item: any) => (
+            <ProductCard product={item} key={item.slug} />
+          ))}
+        </div>
+      ) : (
+        <p></p>
+      )}
+    </div>
+  );
+}
