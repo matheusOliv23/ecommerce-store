fcc11a4afc7754ff8be3fd80bb8ecfcca55bef4e
matheusOliv23 <matheushenri1996@gmail.com>
2025-05-21 06:48:31 -0300

    feat: add btn transitions

--- components/product/add-to-cart.tsx
+++ components/product/add-to-cart.tsx
@@ -1,11 +1,11 @@
 'use client';
 import { Cart, CartItem } from '@/@types';
-import React from 'react';
+import React, { useTransition } from 'react';
 import { Button } from '../ui/button';
 import { useRouter } from 'next/navigation';
 import { addItemToCart, removeItemFromCart } from '@/lib/actions/cart-actions';
 import { toast } from 'sonner';
-import { Plus, Minus } from 'lucide-react';
+import { Plus, Minus, Loader } from 'lucide-react';
 
 export default function AddToCart({
   item,
@@ -15,32 +15,37 @@ export default function AddToCart({
   cart?: Cart;
 }) {
   const router = useRouter();
+  const [isPending, startTransition] = useTransition();
 
   const handleAddToCart = async () => {
-    const res = await addItemToCart(item);
+    startTransition(async () => {
+      const res = await addItemToCart(item);
 
-    if (!res?.success) {
-      toast(res?.message, {
-        description: res?.message,
+      if (!res?.success) {
+        toast(res?.message, {
+          description: res?.message,
+        });
+
+        return;
+      }
+
+      toast(res.message, {
+        action: {
+          label: 'Ver carrinho',
+          onClick: () => router.push('/cart'),
+        },
       });
-
-      return;
-    }
-
-    toast(res.message, {
-      action: {
-        label: 'Ver carrinho',
-        onClick: () => router.push('/cart'),
-      },
     });
   };
 
   const handleRemoveFromCart = async () => {
-    const res = await removeItemFromCart(item.productId);
+    startTransition(async () => {
+      const res = await removeItemFromCart(item.productId);
 
-    toast(res.message, {});
+      toast(res.message, {});
 
-    return;
+      return;
+    });
   };
 
   const existingItem = (cart?.items as CartItem[])?.find(
@@ -50,7 +55,11 @@ export default function AddToCart({
   return existingItem ? (
     <div className='flex items-center gap-6 justify-between'>
       <Button type='button' variant={'outline'} onClick={handleRemoveFromCart}>
-        <Minus />
+        {isPending ? (
+          <Loader className='w-4 h-4 animate-spin' />
+        ) : (
+          <Minus className='w-4 h-4' />
+        )}
       </Button>
       <span>{existingItem.quantity}</span>
       <Button
@@ -59,13 +68,23 @@ export default function AddToCart({
         onClick={handleAddToCart}
         variant={'outline'}
       >
-        <Plus />
+        {isPending ? (
+          <Loader className='w-4 h-4 animate-spin' />
+        ) : (
+          <Plus className='w-4 h-4' />
+        )}
       </Button>
     </div>
   ) : (
     <Button className='w-full text-sm' type='button' onClick={handleAddToCart}>
-      <Plus className='' />
-      Adicionar ao carrinho
+      {isPending ? (
+        <Loader className='w-4 h-4 animate-spin' />
+      ) : (
+        <div>
+          <Plus className='w-4 h-4' />
+          <span className='ml-2'>Adicionar ao carrinho</span>
+        </div>
+      )}
     </Button>
   );
 }
