ef2dd11f93f46da44bcfd8801114185ee1601352
matheusOliv23 <matheushenri1996@gmail.com>
2025-05-23 18:12:26 -0300

    feat: add order validations and types

--- @types/index.ts
+++ @types/index.ts
@@ -2,8 +2,13 @@ import {
   cartItemSchema,
   insertCartSchema,
   shippingAddressSchema,
+
 } from '@/validation/cart-schema';
-import { paymentMethodSchema } from '@/validation/payment';
+import {
+  insertOrderItemSchema,
+  insertOrderSchema,
+  paymentMethodSchema,
+} from '@/validation/payment';
 import { insertProductSchema } from '@/validation/validators';
 import { z } from 'zod';
 
@@ -20,3 +25,19 @@ export type CartItem = z.infer<typeof cartItemSchema>;
 export type ShippingAddress = z.infer<typeof shippingAddressSchema>;
 
 export type PaymentMethod = z.infer<typeof paymentMethodSchema>;
+
+export type Order = z.infer<typeof insertOrderSchema> & {
+  id: string;
+  createdAt: Date;
+  isPaid: boolean;
+  isDelivered: boolean;
+  paidAt: Date | null;
+  deliveredAt: Date | null;
+  orderItems: OrderItem[];
+  user: {
+    name: string;
+    email: string;
+  };
+};
+
+export type OrderItem = z.infer<typeof insertOrderItemSchema>;
--- validation/payment.ts
+++ validation/payment.ts
@@ -1,5 +1,7 @@
 import { PAYMENT_METHODS } from '@/lib/constants';
 import { z } from 'zod';
+import { currency } from './validators';
+import { shippingAddressSchema } from './cart-schema';
 
 export const paymentMethodSchema = z
   .object({
@@ -9,3 +11,27 @@ export const paymentMethodSchema = z
     path: ['type'],
     message: `Payment method must be one of: ${PAYMENT_METHODS.join(', ')}`,
   });
+
+export const insertOrderSchema = z.object({
+  userId: z.string().min(1, 'User ID is required'),
+  itemsPrice: currency,
+  shippingPrice: currency,
+  taxPrice: currency,
+  totalPrice: currency,
+  paymentMethod: z.string().refine((data) => PAYMENT_METHODS.includes(data), {
+    message: `Payment method must be one of: ${PAYMENT_METHODS.join(', ')}`,
+  }),
+  shippingAddress: shippingAddressSchema,
+});
+
+export const insertOrderItemSchema = z.object({
+  productId: z.string().min(1, 'Product ID is required'),
+  name: z.string().min(1, 'Product name is required'),
+  slug: z.string().min(1, 'Product slug is required'),
+  quantity: z
+    .number()
+    .int()
+    .nonnegative('Quantity must be a non-negative integer'),
+  image: z.string().min(1, 'Product image is required'),
+  price: currency,
+});
