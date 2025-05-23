421f51020a87c1b2074523bb8a004d9e930c440c
matheusOliv23 <matheushenri1996@gmail.com>
2025-05-23 08:15:11 -0300

    feat: redirect to login when api error

--- auth.ts
+++ auth.ts
@@ -125,7 +125,22 @@ export const config = {
       return token;
     },
 
-    authorized({ request }: any) {
+    authorized({ request, auth }: any) {
+      const protectedPaths = [
+        /\/shipping-address/,
+        /\/payment-method/,
+        /\/place-order/,
+        /\/profile/,
+        /\/user\/(.*)/,
+        /\/order\/(.*)/,
+        /\/admin\/(.*)/,
+      ];
+
+      const { pathname } = request.nextUrl;
+
+      if (!auth && protectedPaths.some((path) => path.test(pathname)))
+        return false;
+
       if (!request.cookies.get('sessionCartId')) {
         const sessionCartId = crypto.randomUUID();
 
