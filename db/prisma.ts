4456075af63b4090671a80299a4344aebff18a6e
matheusOliv23 <matheushenri1996@gmail.com>
2025-05-24 07:32:10 -0300

    fix: fixing db config

--- db/prisma.ts
+++ db/prisma.ts
@@ -1,19 +1,20 @@
-import { PrismaClient } from '@/lib/generated/prisma';
+import { PrismaClient } from '@/lib/generated/prisma/default';
 import { Pool, neonConfig } from '@neondatabase/serverless';
 import { PrismaNeon } from '@prisma/adapter-neon';
 import ws from 'ws';
 
 // Sets up WebSocket connections, which enables Neon to use WebSocket communication.
 neonConfig.webSocketConstructor = ws;
-const connectionString = `${process.env.NEXT_PUBLIC_DATABASE_URL}`;
+const connectionString = `${process.env.DATABASE_URL}`;
 
 console.log('Connecting to Neon database with connection', connectionString);
 
 // Creates a new connection pool using the provided connection string, allowing multiple concurrent connections.
-const pool = new Pool({ connectionString });
 
 // Instantiates the Prisma adapter using the Neon connection pool to handle the connection between Prisma and Neon.
-const adapter = new PrismaNeon(pool as any);
+const adapter = new PrismaNeon({
+  connectionString,
+});
 
 // Extends the PrismaClient with a custom result transformer to convert the price and rating fields to strings.
 export const prisma = new PrismaClient({ adapter }).$extends({
--- db/seed.ts
+++ db/seed.ts
@@ -1,8 +1,7 @@
-import { PrismaClient } from '@prisma/client';
+import { prisma } from './prisma';
 import sampleData from './sample-data';
 
 async function main() {
-  const prisma = new PrismaClient();
   await prisma.product.deleteMany({});
   await prisma.account.deleteMany({});
   await prisma.session.deleteMany({});
