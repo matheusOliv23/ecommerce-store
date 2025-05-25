dc44f88d3e54ed615c981e0b88ba262d1ac99c34
matheusOliv23 <matheushenri1996@gmail.com>
2025-05-25 09:53:58 -0300

    fix: fixing currency paypl

--- lib/payments/paypal.ts
+++ lib/payments/paypal.ts
@@ -18,7 +18,7 @@ export const paypal = {
         purchase_units: [
           {
             amount: {
-              currency_code: 'BRL',
+              currency_code: 'USD',
               value: price,
             },
           },
